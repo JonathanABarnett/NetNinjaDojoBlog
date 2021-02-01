8e9e42091f4742a687a0c02bd6fe845d1caddfcc
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-02-01 13:10:38 -0600

    Lesson 7 - Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+  const handleClick = (name) => {
+    console.log("you clicked me, " + name);
+  };
+
+  const eventClick = (e) => {
+    console.log(e.target);
+  };
+
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <button onClick={() => handleClick("You Nerd")}>Click Me</button>
+      <button onClick={eventClick}>Event Click</button>
     </div>
   );
 };
