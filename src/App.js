0967954ef391df7e7c883bff10c5767535cb7c8b
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-02-01 11:50:06 -0600

    Lesson 5 - Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  const person = { name: "JB", age: 33 };
+  const link = "https://www.google.com";
+
   return (
     <div className="App">
-      <h1>App Component</h1>
+      {/* Using { }, JSX knows it's dynamic value (variable) 
+      <h1>{title}</h1>
+      <p>Like {likes} times </p>
+      Cannot use object variables 
+      <p>{person}</p>
+      <p>{Math.floor(Math.random() * 10)}</p>
+      <a href={link}>Going to Google</a>
+      */}
+
+      {/* Import Component */}
+      <Navbar />
+
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
