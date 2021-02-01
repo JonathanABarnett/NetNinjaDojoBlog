c71bd25d4d3c073a6a3c68b419f6267d223753bb
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-02-01 13:43:57 -0600

    Lesson 10 - Outputting Lists

--- src/Home.js
+++ src/Home.js
@@ -2,21 +2,23 @@ import { useState } from "react";
 
 const Home = () => {
   // [varName, setVarName] = useState(initialValue);
-  const [name, setName] = useState("JB");
-  const [age, setAge] = useState(33);
-
-  const handleClick = () => {
-    setName("Jonathan");
-    setAge(55);
-  };
+  const [blogs, setBlogs] = useState([
+    { id: 1, title: "Eat a Fish", body: "lorem ipsum...", author: "Cat" },
+    { id: 2, title: "Chew on Bone", body: "lorem ipsum...", author: "Dog" },
+    { id: 3, title: "Dig A Hole", body: "lorem ipsum...", author: "Dog" },
+  ]);
 
   return (
     <div className="home">
-      <h2>Home Page</h2>
-      <p>
-        {name} is {age} years old
-      </p>
-      <button onClick={handleClick}>Click Me</button>
+      {/* array.map((nameOfEachItem) => 
+        { key={nameOfEachItem.id} need a unique key so that react can go through each item}
+    */}
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by: {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
