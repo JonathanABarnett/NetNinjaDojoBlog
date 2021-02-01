a527f1957001f42b72759a4c0b3d8dba2ceb406a
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-02-01 15:29:01 -0600

    Lesson 19 - Handling Fetch Errors

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,18 +1,11 @@
 const BlogList = ({ blogs, title }) => {
-
-
-
   return (
     <div className="blog-list">
-      {/* array.map((nameOfEachItem) => 
-        { key={nameOfEachItem.id} need a unique key so that react can go through each item}
-    */}
       <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by: {blog.author}</p>
-        
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -5,22 +5,32 @@ const Home = () => {
   // [varName, setVarName] = useState(initialValue);
   const [blogs, setBlogs] = useState([]);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState([]);
 
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
         .then((response) => {
+          if (!response.ok) {
+            throw Error("Could not fetch the data for that resource");
+          }
           return response.json();
         })
         .then((data) => {
+            setError([]);
           setBlogs(data);
           setIsPending(false);
+        })
+        .catch((err) => {
+            setIsPending(false);
+          setError(err.message);
         });
     }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="Blogs!" />}
     </div>
