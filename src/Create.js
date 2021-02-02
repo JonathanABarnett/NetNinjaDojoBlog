3652d5744cd27ab5d1a2a2e63e80d9e6ced1414f
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-02-01 19:59:44 -0600

    Lesson 29 - Making a POST Request

--- src/Create.js
+++ src/Create.js
@@ -4,11 +4,23 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("Cat");
+  const [isPending, setIsPending] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
 
-    console.log(blog);
+    setIsPending(true);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      setIsPending(false);
+    });
+
     setTitle("");
     setBody("");
     setAuthor("Cat");
@@ -38,7 +50,8 @@ const Create = () => {
           <option value="Dog">Dog</option>
           <option value="Bird">Bird</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog...</button>}
       </form>
     </div>
   );
